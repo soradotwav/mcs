e38c7b75aca39c3a5770effa21c91a796f233ee6
soradotwav <soradesignz@gmail.com>
2023-05-12 00:09:15 -0700

    Split up classes for readability

--- src/main/java/net/soradotwav/Main.java
+++ src/main/java/net/soradotwav/Main.java
@@ -5,12 +5,15 @@ import java.io.IOException;
 
 public class Main {
 
-    //private static String url = "https://www.metacritic.com/game/playstation-5/trackmania"; // Example game link
+    private static String url = "https://www.metacritic.com/game/playstation-5/trackmania"; // Example game link
     //private static String urlList = "https://www.metacritic.com/browse/games/score/metascore/year/all/filtered"; // Example list link
 
     public static void main(String[] args) throws IOException {
         
         // Empty for testing
+
+        // IMPORTANT: add catch for null score
+
         
     }
 
