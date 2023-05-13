4411d44613d7d03240a862b27a531d28c357bc74
soradotwav <soradesignz@gmail.com>
2023-05-12 18:45:59 -0700

    So much changed

--- src/main/java/net/soradotwav/Main.java
+++ src/main/java/net/soradotwav/Main.java
@@ -6,19 +6,22 @@ import java.util.ArrayList;
 
 public class Main {
 
-    private static String url = "https://www.metacritic.com/game/playstation-5/trackmania"; // Example game link
-    private static String urlList = "https://www.metacritic.com/browse/games/release-date/coming-soon/pc/date"; // Example list link
+    private static String urlList = "https://www.metacritic.com/browse/games/release-date/new-releases/all/date"; // <-- Example list link
 
     public static void main(String[] args) throws IOException {
         
-        // Empty for testing
+        // Empty for testing; below example //
 
-        // IMPORTANT: add catch for null score
-        // IMPORTANT: implement method to only check each page once for all calls
+        mcs gameList = new mcs(); // <-- create new list object
 
-        mcs gameList = new mcs();
+        gameList.initialize(urlList); // <-- initialize scraper and save data in cache
 
-        gameList.initialize(urlList);
+        String gameTitle1 = gameList.getTitles(5); // <-- get a title from the list at an index
+        ArrayList<String> titleList = gameList.getTitles(); // <-- get a list of all the titles
+
+        // Do whatever you want with it //
+
+        // above works for Titles(), ReleaseDates(), Platforms(), Scores() and Links() //
 
         
        
